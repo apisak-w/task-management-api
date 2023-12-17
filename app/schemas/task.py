c5f2c9624cab412a16a508bbd1cc26832630cc7c
apisak-w <apisak.wongkhempetch@gmail.com>
2023-12-17 16:30:38 +0700

    feat: :sparkles: add missing schemas as well as apply it into `routes` file

--- app/schemas/task.py
+++ app/schemas/task.py
@@ -11,6 +11,15 @@ class CreateTaskFormSchema(Schema):
   status = fields.Str(validate=OneOf(ACCEPTED_TASK_STATUS), required=True)
   created_by = fields.String(required=True)
   updated_by = fields.String(required=True)
+
+class CreateTaskJsonSchema(CreateTaskFormSchema):
+    pass
+
+    class Meta:
+        strict = True
+
+class CreateTaskResponseSchema(Schema):
+  message = fields.String()
   
 class UpdateTaskFormSchema(Schema):
   title = fields.String()
@@ -20,13 +29,58 @@ class UpdateTaskFormSchema(Schema):
   created_by = fields.String()
   updated_by = fields.String(required=True)
 
+class UpdateTaskJsonSchema(UpdateTaskFormSchema):
+    pass
+
+    class Meta:
+        strict = True
+
+class UpdateTaskPathSchema(Schema):
+  task_id = fields.Int(required=True)
+
+class UpdateTaskResponseSchema(Schema):
+  message = fields.String()
+
+class UndoTaskActionPathSchema(Schema):
+  task_id = fields.Int(required=True)
+  
+class UndoTaskActionResponseSchema(Schema):
+  message = fields.String()
+
 class UndoTaskActionFormSchema(Schema):
   field = fields.String(required=True)
 
+class UndoTaskActionJsonSchema(UndoTaskActionFormSchema):
+    pass
+
+    class Meta:
+        strict = True
+
 class GetTaskPathSchema(Schema):
   task_id = fields.Int(required=True)
+
+class GetTaskResponseSchema(Schema):
+  created_at = fields.String()
+  created_by = fields.String()
+  description = fields.String()
+  due_date = fields.String()
+  id = fields.Int()
+  is_deleted = fields.Boolean()
+  status = fields.String()
+  title = fields.String()
+  updated_at = fields.String()
+  updated_by = fields.String()
   
 class GetTasksQuerySchema(Schema):
   due_date = fields.Date()
   status = fields.Str(validate=OneOf(ACCEPTED_TASK_STATUS))
   created_by = fields.String()
+
+class GetTasksResponseSchema(Schema):
+  tasks = fields.List(fields.Nested(GetTaskResponseSchema))
+  
+class DeleteTaskPathSchema(Schema):
+  task_id = fields.Int(required=True)
+  
+class DeleteTaskResponseSchema(Schema):
+  message = fields.String()
--- app/task/routes.py
+++ app/task/routes.py
@@ -1,19 +1,16 @@
 from flask import request, jsonify
 from marshmallow import ValidationError
-from flasgger import Swagger, swag_from, Schema
 from flasgger_marshmallow import swagger_decorator
 
 from app.models.task import Task
-from app.schemas.task import CreateTaskFormSchema, GetTaskPathSchema, GetTasksQuerySchema, UndoTaskActionFormSchema, UpdateTaskFormSchema
-
+from app.schemas.task import CreateTaskFormSchema, CreateTaskJsonSchema, CreateTaskResponseSchema, DeleteTaskPathSchema, DeleteTaskResponseSchema, GetTaskPathSchema, GetTaskResponseSchema, GetTasksQuerySchema, UndoTaskActionFormSchema, UndoTaskActionJsonSchema, UndoTaskActionPathSchema, UndoTaskActionResponseSchema, UpdateTaskFormSchema, UpdateTaskJsonSchema, UpdateTaskPathSchema, UpdateTaskResponseSchema
 from app.task import bp
 
 SWAGGER_DECORATOR_OPTIONS = {
     'tags': ['Task']
 }
 
-
-@swagger_decorator(form_schema=CreateTaskFormSchema, tags=SWAGGER_DECORATOR_OPTIONS['tags'])
+@swagger_decorator(json_schema=CreateTaskJsonSchema, response_schema={201: CreateTaskResponseSchema}, tags=SWAGGER_DECORATOR_OPTIONS['tags'])
 @bp.route('', methods=['POST'])
 def create_task():
     data = request.get_json()
@@ -30,7 +27,7 @@ def create_task():
     
     return jsonify({'message': 'Task created successfully'}), 201
 
-@swagger_decorator(query_schema=GetTasksQuerySchema, tags=SWAGGER_DECORATOR_OPTIONS['tags'])
+@swagger_decorator(query_schema=GetTasksQuerySchema, response_schema={200: CreateTaskFormSchema}, tags=SWAGGER_DECORATOR_OPTIONS['tags'])
 @bp.route('', methods=['GET'])
 def get_tasks():
     due_date = request.args.get('due_date')
@@ -41,13 +38,13 @@ def get_tasks():
     
     return jsonify(tasks_list)
 
-@swagger_decorator(path_schema=GetTaskPathSchema, tags=SWAGGER_DECORATOR_OPTIONS['tags'])
+@swagger_decorator(path_schema=GetTaskPathSchema, response_schema={200: GetTaskResponseSchema}, tags=SWAGGER_DECORATOR_OPTIONS['tags'])
 @bp.route('/<int:task_id>', methods=['GET'])
 def get_task(task_id):
     task_data = Task.get_task(task_id)
     return jsonify(task_data)
 
-@swagger_decorator(form_schema=UpdateTaskFormSchema, tags=SWAGGER_DECORATOR_OPTIONS['tags'])
+@swagger_decorator(path_schema=UpdateTaskPathSchema, json_schema=UpdateTaskJsonSchema, response_schema={200: UpdateTaskResponseSchema}, tags=SWAGGER_DECORATOR_OPTIONS['tags'])
 @bp.route('/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
     request_body = request.get_json()
@@ -64,7 +61,7 @@ def update_task(task_id):
     
     return jsonify({'message': 'Task updated successfully'})
 
-@swagger_decorator(form_schema=UndoTaskActionFormSchema, tags=SWAGGER_DECORATOR_OPTIONS['tags'])
+@swagger_decorator(path_schema=UndoTaskActionPathSchema, json_schema=UndoTaskActionJsonSchema, response_schema={200: UndoTaskActionResponseSchema}, tags=SWAGGER_DECORATOR_OPTIONS['tags'])
 @bp.route('/<int:task_id>/undo', methods=['POST'])
 def undo_task_action(task_id):
     request_body = request.get_json()
@@ -81,7 +78,7 @@ def undo_task_action(task_id):
     
     return jsonify({'message': 'Task undid successfully'})
 
-@swagger_decorator(tags=SWAGGER_DECORATOR_OPTIONS['tags'])
+@swagger_decorator(path_schema=DeleteTaskPathSchema, response_schema={200: DeleteTaskResponseSchema}, tags=SWAGGER_DECORATOR_OPTIONS['tags'])
 @bp.route('/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
     Task.delete_task(task_id)
